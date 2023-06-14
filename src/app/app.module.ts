9bb6ff678e0b4ec126486d6e72b04310b7d6ff86
thiti-lim <thiti-lim@proton.me>
2023-06-14 16:32:33 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './components/material/material.module';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { JobHistoryComponent } from './job/job-history/job-history.component';
+import { AddJobComponent } from './job/add-job/add-job.component';
+import { JobDetailComponent } from './job/job-detail/job-detail.component';
+import { HomeComponent } from './home/home.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'job/history', component: JobHistoryComponent },
+  { path: 'job', redirectTo: 'job/history', pathMatch: 'full' },
+  { path: 'customer/list', component: CustomerListComponent },
+  { path: 'customer', redirectTo: 'customer/list', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent,
+    JobHistoryComponent,
+    AddJobComponent,
+    JobDetailComponent,
+    HomeComponent,
+    CustomerListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/job/job-history/job-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-history',
+  templateUrl: './job-history.component.html',
+  styleUrls: ['./job-history.component.css']
+})
+export class JobHistoryComponent {
+
+}
